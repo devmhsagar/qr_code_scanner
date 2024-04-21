9225e30cefb2981c5e9f1679f73e338f136ea171
mmehedihasan <mmehedihasansagar@gmail.com>
2024-04-21 11:20:56 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/qr_code_scanner_generator/qr_code_scanner_generator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.qr_code_scanner_generator.qr_code_scanner_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QR Code Scanner and Generator',
+      home:  HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QR Code Scanner and Generator'), backgroundColor: Colors.blue,
+      ),
+      body: const Center(
+        child: Text('QR Code Scanner and Generator'),
+      ),
+    );
+  }
+}
+
