7ba3f49c1fc12b5a1fba9899f517d88dec2bca09
mmehedihasan <mmehedihasansagar@gmail.com>
2024-04-25 14:56:17 +0600

    Qr Code Scanner Complete

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,41 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:qr_code_scanner/scan_qr_code.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key});
+  final String code;
+  final Function() closeScanner;
+
+  const ResultScreen(
+      {super.key, required this.code, required this.closeScanner});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgColor,
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            closeScanner();
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: Colors.black87,
+          ),
+        ),
         centerTitle: true,
-        title: const Text('QR Scanner',
+        title: const Text(
+          'QR Scanner',
           style: TextStyle(
             fontSize: 18,
             fontWeight: FontWeight.bold,
             color: Colors.black87,
             letterSpacing: 1,
-          ),),
+          ),
+        ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Scanner Result',
+            QrImageView(data: code, version: QrVersions.auto, size: 150),
+            const Text(
+              'Scanner Result',
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
                 color: Colors.black54,
                 letterSpacing: 1,
-              ),),
-            SizedBox(height: 10),
-            Text('Result',
-              style: TextStyle(
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              code,
+              style: const TextStyle(
                 fontSize: 16,
                 color: Colors.black87,
                 letterSpacing: 1,
-              ),),
+              ),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 100,
+              height: 48,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: () {
+                  Clipboard.setData(ClipboardData(text: code),);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Copied to clipboard'),
+                      duration: Duration(seconds: 2),
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Copy',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black87,
+                    letterSpacing: 1,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
--- lib/scan_qr_code.dart
+++ lib/scan_qr_code.dart
@@ -1,74 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:qr_code_scanner/result_screen.dart';
+import 'package:qr_scanner_overlay/qr_scanner_overlay.dart';
 
 const bgColor = Color(0xfffafafa);
 
 class ScanQrCode extends StatefulWidget {
-  const ScanQrCode({super.key});
+  const ScanQrCode({Key? key}) : super(key: key);
 
   @override
   State<ScanQrCode> createState() => _ScanQrCodeState();
 }
 
 class _ScanQrCodeState extends State<ScanQrCode> {
+  MobileScannerController cameraController = MobileScannerController();
+  bool isScannedCompleted = false;
+
+  void closeScanner() {
+    setState(() {
+      isScannedCompleted = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: bgColor,
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('QR Scanner',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Colors.black87,
-              letterSpacing: 1,
-            ),),
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'QR Scanner',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+            letterSpacing: 1,
+          ),
         ),
-        body: Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                const Expanded(
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Place the QR Code in the Area',
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black87,
-                              letterSpacing: 1,
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Place the QR Code in the Area',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Scan QR Code Result Automatic Generated',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Stack(
+                children: [
+                  MobileScanner(
+                    allowDuplicates: true,
+                    controller: cameraController,
+                    onDetect: (barcode, args) {
+                      if (!isScannedCompleted) {
+                        String code = barcode.rawValue ?? '---'; // Access the scanned data from the 'code' property
+                        setState(() {
+                          isScannedCompleted = true;
+                        });
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ResultScreen(
+                              code: code,
+                              closeScanner: closeScanner,
+                            ),
                           ),
-                        ),
-                       SizedBox(height: 10),
-                        Text('Scan QR Code Result Automatic Generated',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black54,
-                        ),
-                        ),
-                      ],),
-                ),
-                Expanded(
-                  flex: 4,
-                  child: Container(
-                    color: Colors.green,
+                        );
+                      }
+                    },
+                  ),
+                  QRScannerOverlay(overlayColor: bgColor.withOpacity(0.5),)
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                alignment: Alignment.center,
+                child: const Text(
+                  'Developed by: MH Sagar',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black87,
+                    letterSpacing: 1,
                   ),
                 ),
-                Expanded(
-                  child: Container(
-                    alignment: Alignment.center,
-                    child: const Text('Developed by: MH Sagar',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.black87,
-                        letterSpacing: 1,
-                      ),),
-                  ),
-                ),
-              ],
-            )));
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
